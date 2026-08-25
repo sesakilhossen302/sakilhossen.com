fb779a79af777bdcfaf9049408dc09e086f0fb9c
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-25 16:28:09 +0600

    Remove font_awesome_flutter package to ensure web build compiles successfully

--- portfolio/lib/sections/hero_section.dart
+++ portfolio/lib/sections/hero_section.dart
@@ -1,6 +1,5 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
