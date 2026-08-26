3b2a19d0f0a53eab19fe944fda8dee7bcb0a9436
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-26 10:41:54 +0600

    Deploy updated Flutter Web production build with video background and profile image fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1035420711" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2374130434" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
