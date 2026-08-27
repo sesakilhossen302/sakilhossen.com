ab42f191bceccc750de12608e74c438e0dafc9f4
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-27 14:46:10 +0600

    Deploy updated Flutter Web production build with multi-video playlist UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "66274211" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1166534171" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
